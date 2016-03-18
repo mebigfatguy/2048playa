951a40a80e16c6588dc659f944e8009e4ee7c825
Dave Brosius <dbrosius@mebigfatguy.com>
2016-03-17 20:10:27 -0400

    update cp dates

--- src/com/mebigfatguy/twenty48playa/OptionComparator.java
+++ src/com/mebigfatguy/twenty48playa/OptionComparator.java
@@ -1,6 +1,6 @@
 /** 2048 playa - a 2048 autonomous player. 
- * Copyright 2014-2015 MeBigFatGuy.com 
- * Copyright 2014-2015 Dave Brosius 
+ * Copyright 2014-2016 MeBigFatGuy.com 
+ * Copyright 2014-2016 Dave Brosius 
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
